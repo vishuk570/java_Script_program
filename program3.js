fde0a6eb57d6df3aa7a1d01622cd64815f2b7524
vishu <vishuk570@gmail.com>
2024-02-05 16:02:44 +0530

    reverse string without using inbuild function

---
+++ program3.js
@@ -0,0 +1,27 @@
+// Reverse a String
+// input = "Hello World"
+// output = "dlroWolleH"
+
+// this is using inbuild function.
+// function reverseString(str){
+//     // convert string to array => split
+//     // convert array to string => join
+//     const strr = str.split('').reverse().join('').replace(' ','');
+//     return strr
+// }
+// const result = reverseString("Hello World");
+// console.log(result);
+// --------------------------------------
+
+// without using inbuild function.
+function reverseString(str){
+    let reversed = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        if (str[i] !== ' ') {
+            reversed += str[i];
+        }
+    }
+    return reversed;
+}
+const result = reverseString("Hello World");
+console.log("result",result);
