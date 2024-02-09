95c43218ae13919aea68eae6573bec71313baadb
vishu <vishuk570@gmail.com>
2024-02-09 16:36:47 +0530

    Events and Scope in JavaScripts

---
+++ 1_Events.js
@@ -0,0 +1,2 @@
+// Explain Event Bubbling and Event Capturing:
+// How do they differ, and how can you control them?
---
+++ 2_Scope.js
@@ -0,0 +1,3 @@
+// What is scope in JavaScript?
+// What is the difference between global scope and local scope?
+// How do closures work, and why are they important?
