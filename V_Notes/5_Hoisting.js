81feb6c5e7f78e535e7842211f79cdc441f9d990
vishu <vishuk570@gmail.com>
2024-02-16 12:06:19 +0530

    stuctured

---
+++ V_Notes/5_Hoisting.js
@@ -0,0 +1,14 @@
+// JavaScript Hoisting
+// Hoisting is JavaScript's default behavior of moving declarations to the top.
+
+// JavaScript Declarations are Hoisted
+
+var x; // Declare x
+
+var y; // Declare y
+y = 5; // Assign 5 to y
+
+// JavaScript Initializations are Not Hoisted
+
+var x = 5; // Initialize x
+var y = 7; // Initialize y
