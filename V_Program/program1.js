cd8df20db9a40a39b5b6525626757b08fe29a6c9
vishu <vishuk570@gmail.com>
2024-06-26 10:53:22 +0530

    format change

--- V_Program/program1.js
+++ V_Program/program1.js
@@ -2,26 +2,26 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
 // function Add two number
-function Add(a,b){
-    return a+b;
+function Add(a, b) {
+    return a + b;
 }
 
 // Take input for the first number
-rl.question('Enter the firts Number:',(num1)=>{
+rl.question('Enter the firts Number:', (num1) => {
     // Take input for the Second number
-    rl.question('Enter the second Number',(num2)=>{
+    rl.question('Enter the second Number', (num2) => {
         num1 = parseFloat(num1);
         num2 = parseFloat(num2);
 
         // Check if the inputs are valid numbers
-        if(!isNaN(num1) && !isNaN(num2)){
+        if (!isNaN(num1) && !isNaN(num2)) {
             // Perform addition
-            let result = Add(num1,num2);
+            let result = Add(num1, num2);
 
             // Display the result
             console.log(`The sum of ${num1} and ${num2} is: ${result}`);
