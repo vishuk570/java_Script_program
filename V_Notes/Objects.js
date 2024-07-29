93ddea90c926dc1b5c0999d99102b657975e9e50
vishu <vishuk570@gmail.com>
2024-07-29 11:50:21 +0530

    object in js

---
+++ V_Notes/Objects.js
@@ -0,0 +1,34 @@
+// JavaScript Objects
+// Objects are one of JavaScript's data types.
+// Objects are used to store key/value (name/value) collections.
+
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+};
+console.log(person.age , person.eyeColor, person.firstName, person.lastName); //50 blue John Doe
+
+// 1. assign() :	Copies properties from a source object to a target object
+// 2. constructor : 	Returns the function that created an object's prototype
+// 3. create() :	Returns an object created from an existing object
+// 4. defineProperties() :	Adds or changes properties
+// 5. defineProperty() : 	Adds or changes a property
+// 6. entries() :	Returns an array of the key/value pairs of an object
+// 7. freeze() :	Prevents any changes to an object
+// 8. fromEntries() :	Returns an object created from an iterable list of key/value pairs
+// 9. getOwnPropertyDescriptor() :	Returns an array of the keys of an object
+// 10. getOwnPropertyDescriptors() :	Returns an array of the keys of an object
+// 11. getOwnPropertyNames() :	Returns an array of the keys of an object
+// 12. groupBy() :	Groups object elements according to returned callback values
+// 13. isExtensible() :	Returns true if an object is extensible
+// 14. isFrozen() :	Returns true if an object is frozen
+// 15. isSealed() :	Returns true if an object is sealed
+// 16. keys() :	Returns an array of the keys of an object
+// 17. preventExtensions() :	Prevents adding new properties to an object
+// 18. prototype :	Let you to add properties and methods to JavaScript objects
+// 19. seal() :	Prevents adding new or deleting existing object properties
+// 20. toString() :	Converts an object to a string and returns the result
+// 21. valueOf() :	Returns the primitive value of an object
+// 22. values() :	Returns an array of the property values of an object
