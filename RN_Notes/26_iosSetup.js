23d1e1fbf8a1673916f653f3a1171666c6cb55d6
vishu <vishuk570@gmail.com>
2024-08-26 17:24:34 +0530

    proccess of ios set up in for react native projects

---
+++ RN_Notes/26_iosSetup.js
@@ -0,0 +1,50 @@
+// we need to xcode and developer account for run the app in ios.
+
+// 1. open keychain access
+// 2. Click keychain access -> Certificate Asistant -> Request a certificate from a certificate Authority...
+// 3. create 2 certSigingRequest -> one is dev and second one is dist.
+
+// aftre that
+
+// 1. signin with developer account on https://appleid.apple.com/sign-in
+// 2. click Account -> certificates and create 2 certificate using certSigingRequest for dev and dist.
+// and download the certificate and double on that.
+
+// 3. click Identifiers and create identifiers with unique bundle ID.
+// 4. Add device udid of device in which we will run the app.
+// 5. click profiles -> create profile with unique identifiers for dev and dist.
+
+// 6. open project's ios folder in xcode.
+// 7. setup the bundle identifier and provisioning profile using the developer account credential in xcode.
+// 8. Now we can run the project in ios.
+
+
+// ===========================================================================
+
+// Proccess to upload the app on app store and play store
+
+// Need some important assets listing below.
+
+// ⦁	Business name/Organization name (Like Apple Inc.)
+// ⦁	App Icon (PNG 512px by 512px) and screenShots.
+// ⦁	Banner Image (PNG 1,024px by 500px).
+// ⦁	App Name (The name will be reviewed before it is made available on the App Store.)
+// ⦁	Subtitle (The subtitle will be reviewed before it is made available on the App Store.): 
+// ⦁	Description (A description of your app, detailing features and functionality.): 
+// ⦁	Promotional Text (Promotional text lets you inform your App Store visitors of any current app features without requiring an updated submission. This text will appear above your description on the App Store for customers with devices running iOS 11 or later, and macOS 10.13 or later.)
+// ⦁	Keywords (Include one or more keywords that describe your app. Keywords make App Store search results more accurate. Separate keywords with an English comma, Chinese comma, or a mix of both.)
+// ⦁	Support URL (This can be contact us page.)
+// ⦁	Marketing URL (optional)
+// ⦁	Privacy Policy URL 
+// ⦁	Copyright (Like Copyright © 2022 Apple Inc. All rights reserved.)
+// ⦁	Contact Information
+// ⦁	First Name
+// ⦁	Last Name
+// ⦁	Phone
+// ⦁	Email
+// ⦁	Website
+// ⦁	Notes (Additional information about your app that can help during the review process. Include information that may be needed to test your app, such as app-specific settings.)
+// ⦁	Price: Free or amount. if amount please let us know amount in dollar
+// ⦁	Availability (Countries)
+// ⦁	Category (The category will be reviewed before it is made available on the App Store.) (Select 2 from below)
+ 
